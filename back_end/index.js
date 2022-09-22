c0e3f54e7b532f4875bcfa3f982ed0517e890556
Chad Tao <chenchen408@gmail.com>
2022-09-22 10:49:02 -0700

    ok

--- back_end/index.js
+++ back_end/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas/index.js');
-const db = require('./config/connection.js');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas/index.js");
+const db = require("./config/connection.js");
 const { authMiddleware } = require("./utils/auth");
 const { Server } = require("socket.io");
-const http = require("http")
+const http = require("http");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,32 +17,32 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-const ioServer = http.createServer(app)
+const ioServer = http.createServer(app);
 
 const io = new Server(ioServer, {
   cors: {
     origin: "http://localhost:3000",
-    method: ["GET", "POST"]
-  }
-})
+    method: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   console.log(`User connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
-    socket.join(data)
-    console.log(`User with ID: ${socket.id} joined room: ${data}`)
-  })
+    socket.join(data);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+  });
 
   socket.on("send_message", (data) => {
     console.log(data);
-    socket.to(data.room).emit("receive_message", data)
-  })
+    socket.to(data.room).emit("receive_message", data);
+  });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected", socket.id)
-  })
-})
+    console.log("User disconnected", socket.id);
+  });
+});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -55,29 +55,24 @@ app.get("/", (req, res) => {
 });
 
 //FOR TESTING PURPOSES
-app.get("/whoami", (req,res)=> {
-  res.send("<h1>THIS IS NAFTIE, THE ILLEST SOCIAL MEDIA PLATFORM!</h1>")
-})
-
+app.get("/whoami", (req, res) => {
+  res.send("<h1>THIS IS NAFTIE, THE ILLEST SOCIAL MEDIA PLATFORM!</h1>");
+});
 
 const startApolloServer = async (typeDefs, resolvers) => {
-await server.start();
-server.applyMiddleware({ app });
-
-    db.once("open", () => {
-      ioServer.listen(PORT, () => {
-        console.log(`API server running on port ${PORT}!`);
-        console.log(
-          `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-        );
-      });
+  await server.start();
+  server.applyMiddleware({ app });
+  db.once("open", () => {
+    ioServer.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
     });
-
+  });
 };
 
 startApolloServer(typeDefs, resolvers);
 
-
-
-  //This useFindAndModify is causing the error it is deprecated from mongoose version 6
+//This useFindAndModify is causing the error it is deprecated from mongoose version 6
 // mongoose.set("useFindAndModify", false) it was removed and it is not needed anymore;
