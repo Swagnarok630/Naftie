47bdec0c62f268078f222c9e3b4f7ebc80dbbee5
Chad Tao <chenchen408@gmail.com>
2022-09-22 11:14:16 -0700

    ok

--- back_end/index.js
+++ back_end/index.js
@@ -50,15 +50,17 @@ app.use(cors());
 //Problably causing the error
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../neftie_frontend/build/index.html"));
-});
 
 //FOR TESTING PURPOSES
 app.get("/whoami", (req, res) => {
   res.send("<h1>THIS IS NAFTIE, THE ILLEST SOCIAL MEDIA PLATFORM!</h1>");
 });
 
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../neftie_frontend/build/index.html"));
+  });
+}
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
